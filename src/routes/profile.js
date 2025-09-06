34a16dc1f07f8a8fbf15d708d0472828bb7b89e3
dineshbezawada <dineshbezawada@gmail.com>
2025-09-06 21:54:23 +0530

    updatePassword api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateProfile } = require("../utils/validation");
 const profileRouter = express.Router();
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile/view", userAuth, (req, res) => {
   try {
@@ -21,9 +22,37 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     console.log(req.body);
     Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
     await user.save();
-    res.send({ message: `${user.firstName} Profile Updated Successfully`, data : user });
+    res.send({
+      message: `${user.firstName} Profile Updated Successfully`,
+      data: user,
+    });
   } catch (err) {
     res.status(400).send(`Error : ${err.message}`);
+  }
+});
+
+profileRouter.post("/profile/updatePassword", userAuth, async (req, res) => {
+  try {
+    const { currentPassword, updatedPassword } = req.body;
+        if(currentPassword === updatedPassword){
+            throw new Error("Old and New password should not be same");
+        }
+    const user = req.user;
+    const userBeforeUpdate = JSON.parse(JSON.stringify(user));
+    const isMatched = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatched) {
+      throw new Error("Password is Incorrect");
+    }
+    const modifiedPasswordHash = await bcrypt.hash(updatedPassword, 10);
+    user.password = modifiedPasswordHash;
+    await user.save();
+    res.send({
+      message: `${user.firstName} password Updated successfully`,
+      previous: userBeforeUpdate.password,
+      after: user.password,
+    });
+  } catch (err) {
+    res.status(500).send(`Error : ${err.message}`);
   }
 });
 
